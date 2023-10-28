335c22320e1f45c5229218bf3977e767214af866
alanmasu <alan.masutti@gmail.com>
2023-10-28 12:48:56 +0200

    Aggiunto esp-fs-webserver [LENTO]
      - Ci mette troppo tempo per fare un loop, da seriale si vede che ci mette circa 1 secondo per fare un loop

--- src/Tabellone/tabellone.cpp
+++ src/Tabellone/tabellone.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <ArduinoOTA.h>
+//#include <ArduinoOTA.h>
 #include <Ticker.h>
 #include <SPI.h>
 #include <EEPROM.h>
@@ -93,9 +93,9 @@ byte minuti;        //Minuti
 byte ore;           //Ore
 bool RTC;           //Stato di configurazione RTC
 
-//WiFi
-char ssid[] = "Tabellone";
-char pass[] = "Tabellone";
+// //WiFi
+// char ssid[] = "Tabellone";
+// char pass[] = "Tabellone";
 
 
 //Implementazione di metodi di struct
@@ -333,58 +333,58 @@ void displayWrite() {
   falli2.write(valori.val[6]);
 }
 
-void initWiFi(){
+// void initWiFi(){
   
-  //Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_AP_STA);
-  WiFi.softAP(ssid, pass);
-  Serial.println("WiFi AP started");
-  Serial.println(WiFi.softAPIP());
-  Serial.println(WiFi.macAddress());
+//   //Set device as a Wi-Fi Station
+//   WiFi.mode(WIFI_AP_STA);
+//   WiFi.softAP(ssid, pass);
+  // Serial.println("WiFi AP started");
+//   Serial.println(WiFi.softAPIP());
+//   Serial.println(WiFi.macAddress());
 
-}
+// }
 
 void initOTA() {
   // Port defaults to 3232
-  ArduinoOTA.setPort(3232);
+  // ArduinoOTA.setPort(3232);
 
-  // Hostname defaults to esp3232-[MAC]
-  // ArduinoOTA.setHostname("Tabellone");
+  // // Hostname defaults to esp3232-[MAC]
+  // // ArduinoOTA.setHostname("Tabellone");
 
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
+  // // No authentication by default
+  // // ArduinoOTA.setPassword("admin");
 
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+  // // Password can be set with it's md5 value as well
+  // // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
+  // ArduinoOTA
+  //   .onStart([]() {
+  //     String type;
+  //     if (ArduinoOTA.getCommand() == U_FLASH)
+  //       type = "sketch";
+  //     else // U_SPIFFS
+  //       type = "filesystem";
 
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
+  //     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+  //     Serial.println("Start updating " + type);
+  //   })
+  //   .onEnd([]() {
+  //     Serial.println("\nEnd");
+  //   })
+  //   .onProgress([](unsigned int progress, unsigned int total) {
+  //     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  //   })
+  //   .onError([](ota_error_t error) {
+  //     Serial.printf("Error[%u]: ", error);
+  //     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+  //     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+  //     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+  //     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+  //     else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  //   });
 
-  ArduinoOTA.begin();
+  // ArduinoOTA.begin();
 }
 
 bool initESP_NOW(esp_now_peer_info_t* peerInfo) {
@@ -392,6 +392,8 @@ bool initESP_NOW(esp_now_peer_info_t* peerInfo) {
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
 
+  WiFi.mode(WIFI_AP_STA);
+  
   //Disabilito il controllo potenza WiFi
   esp_wifi_set_ps(WIFI_PS_NONE);
 
@@ -575,9 +577,9 @@ void tik() {
 }
 
 //----------------------------------------------------------------------------------- OTA
-void serverLoop() {
-  ArduinoOTA.handle();
-}
+// void serverLoop() {
+//   // ArduinoOTA.handle();
+// }
 
 //----------------------------------------------------------------------------------- CORE
 // String readSerial(String &str);
