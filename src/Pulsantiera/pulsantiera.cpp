559b1e6434b95c17ebfc3a91608dfac9481e3eaf
alanmasu <alan.masutti@gmail.com>
2023-11-01 16:13:52 +0100

    Tolti dei refusi dello spostamento dei tipi comuni e resa inline la funzione dell'OTA

--- src/Pulsantiera/pulsantiera.cpp
+++ src/Pulsantiera/pulsantiera.cpp
@@ -39,86 +39,6 @@ bool serialModeEnable = false;
 char ssid[] = "Tabellone";
 char pass[] = "Tabellone";
 
-//Implementazione di metodi di struct
-//Comandi
-Comandi::Comandi() {
-  for (byte i = 0; i < 17; i++) {
-    state[i] = 0;
-  }
-}
-void Comandi::print()const {
-  for (byte i = 0; i < 16; i++) {
-    Serial.print(state[i]); Serial.print(".");
-  }
-  Serial.print(state[16]);
-
-}
-void Comandi::println()const {
-  print();
-  Serial.println();
-}
-
-//Valori
-Valori::Valori() {
-  for (byte i = 0; i < 9; i++) {
-    val[i] = 0;
-  }
-  stato = stop;
-  mode = tabellone;
-  modeImpostata = false;
-}
-
-void Valori::print(bool whitConf)const {
-  for (byte i = 0; i < 8; i++) {
-    Serial.print(val[i]); Serial.print(".");
-  }
-  Serial.print(val[8]);
-  if (whitConf) {
-    String str = "\tStato:";
-    switch (stato) {
-      case stop:
-        str += "stop\t";
-        break;
-      case run:
-        str += "run\t";
-        break;
-    }
-    str += "Modalita': ";
-    switch (mode) {
-      case tabellone:
-        str += "tabellone\t";
-        break;
-      case orologio:
-        str += "orologio\t";
-        break;
-    }
-    str += "Mode Impostata: ";
-    str += modeImpostata;
-    Serial.print(str);
-  }
-}
-void Valori::println(bool whitConf)const {
-  print(whitConf);
-  Serial.println();
-}
-bool Valori::operator==(const Valori &val2) {
-  for (byte i = 0; i < 9; i++) {
-    if (val[i] != val2.val[i]) {
-      return false;
-    }
-  }
-  if (stato != val2.stato) {
-    return false;
-  }
-  if (mode != val2.mode) {
-    return false;
-  }
-  if (modeImpostata != val2.modeImpostata) {
-    return false;
-  }
-  return true;
-}
-
 //Dichiarazioni delle funizioni
 //Inizializzazione
 
@@ -425,8 +345,4 @@ void connectionErrorHandle() {
     delay(5000);
     ESP.restart();
   }
-}
-
-void serverLoop() {
-  ArduinoOTA.handle();
 }
