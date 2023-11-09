cbfd2c179fbd74d6767f6d5422f1d370d341b1fd
alanmasu <alan.masutti@gmail.com>
2023-11-09 18:16:04 +0100

    Aggiunto pico in CSS +  aggiunta artella ino per upload littleFS + onload funtion
      - Aggiunta la funzione al caricamento della pagina web durante l'OTA,
        va aggiunto l'handler lato server!

--- src/Pulsantiera/pulsantiera.cpp
+++ src/Pulsantiera/pulsantiera.cpp
@@ -390,7 +390,6 @@ void evaluateData() {
   }
   if(recv.mode != OTA && recv.mode != tabStatus.mode){
     Serial.println("EXIT OTA MODE");
-    delay(10000);
     exitOtaMode();
   }
   tabStatus = recv;
